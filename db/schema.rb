14d4cf70490451316ac230625adeb04717df47ee
Oleg Oleynik <xakep0k@mail.ru>
2018-05-31 13:08:12 +0300

    add feedback form

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180522082856) do
+ActiveRecord::Schema.define(version: 20180531100303) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "contacts", force: :cascade do |t|
+    t.string "email"
+    t.string "name"
+    t.string "subject"
+    t.string "message"
+  end
 
   create_table "tastes", force: :cascade do |t|
     t.string "index"
